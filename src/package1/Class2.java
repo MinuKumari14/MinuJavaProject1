495a55b2214b8e905fa4e539d76e43b1310970e4
MinuKumari14 <minuchy1406@gmail.com>
2022-07-29 08:15:33 +0530

    Main method change

---
+++ src/package1/Class2.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("Test class2 A");
+		System.out.println("Test class2.... A");
+		System.out.println("Test class2 change A");
+
+	}
+
+}
